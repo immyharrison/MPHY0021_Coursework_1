d41cf9619a572046163c1ae7dda5e1064c4e189f
immyharrison <zcqsarr@ucl.ac.uk>
2020-12-10 08:53:26 +0000

    test covid data structure check

--- test_process_covid.py
+++ test_process_covid.py
@@ -1,2 +1,35 @@
 from process_covid import match_age_bins ,check_json_strucutre, hospital_vs_confirmed ,covid_data, generate_data_plot_confirmed, load_covid_data, compute_running_average, simple_derivative, cases_per_population_by_age
 import pytest 
+
+def test_check_json_strucutre():
+    input_data = {"metadata":{"age_binning": {"hospitalizations": ["0-24","25-49","50-74","75-"],}},},
+    {"evolution":{"2020-03-16": {"hospitalizations": {
+        "hospitalized": {
+          "new": {
+            "all": None,
+            "male": 28,
+            "female": 44,
+            "age": [
+              15,
+              23,
+              21,
+              13
+            ]
+    }}}}}}
+    {"epidemiology": {
+        "confirmed": {
+          "new": {
+            "all": None,
+            "male": 66,
+            "female": 78,
+            "age": [
+              29,
+              49,
+              43,
+              23
+            ]
+          },
+      }}}
+    actual = check_json_strucutre(input_data)
+    expected = 'Error: ','weather', ' not found in the loaded data'
+    assert actual == expected
