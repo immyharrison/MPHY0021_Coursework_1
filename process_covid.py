d2e6680b928216ddb7ef26499fa8447b3889fb4f
immyharrison <zcqsarr@ucl.ac.uk>
2020-12-10 07:58:35 +0000

    2.1 load json file

--- process_covid.py
+++ process_covid.py
@@ -1,7 +1,29 @@
 # FIXME add needed imports
+# import json file 
+import json
+# import to get file path 
+import os 
+
+current_filepath = os.getcwd()
+
 
 def load_covid_data(filepath):
-    raise NotImplementedError
+      list_files = os.listdir(filepath)
+    #check for all files in folder
+    check_files_counter = 0
+    for fname in list_files: 
+        # if file type is json file
+        if fname.endswith('.json'):
+            # open file
+            open_covid_data = open(fname)
+            #load file
+            covid_data_json = json.load(open_covid_data)
+            #load file as string
+            covid_data_str = json.dumps(covid_data_json)
+            #print(covid_data_str)
+            #check title of file fits schema
+            # the catagories being checked if present
+    
 
 def cases_per_population_by_age(input_data):
     raise NotImplementedError
