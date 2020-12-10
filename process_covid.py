9da04a809cab6066d91882c9b3732babb267aa58
immyharrison <zcqsarr@ucl.ac.uk>
2020-12-10 13:56:17 +0000

    fix functions being classified as function

--- process_covid.py
+++ process_covid.py
@@ -9,6 +9,86 @@ current_filepath = os.getcwd()
 # load matplotlib to plot graphs 
 import matplotlib.pyplot as plt
 
+def match_age_bins(binning_dic_hosp,binning_dic_pop):
+    binning_dic_hosp = ['0-9','10-19','20-29','30-39','40-49','50-']
+    binning_dic_pop = ['0-19', '20-39', '40-']
+    count_hosp = 0
+    count_pop = 0 
+    hosp_age_bin_resort  = {}
+    pop_age_bin_resort = {}
+    new_age_bins_resort = []
+    len_binning_dic_pop  = len(binning_dic_pop)
+    len_binning_dic_hosp = len(binning_dic_hosp)
+    while count_hosp < len_binning_dic_hosp and count_pop < len_binning_dic_pop:
+            # upper limit for hospital age bin
+            current_hosp_bin = (binning_dic_hosp[count_hosp]) 
+            split = current_hosp_bin.split('-')
+            limit_upper_hosp = (current_hosp_bin.partition('-')[2])
+            limit_lower_hosp = (current_hosp_bin.partition('-')[0])
+            # upper limit as number
+            if limit_upper_hosp != '':
+                limit_upper_hosp = int(limit_upper_hosp)
+            #upper limit not defined so define as 100 max age 
+            elif limit_upper_hosp == '' or limit_upper_hosp == ' ':
+                limit_upper_hosp = int(100)
+            if limit_lower_hosp != '':
+                limit_lower_hosp = int(limit_lower_hosp)
+            # upper value for population age bin
+            current_pop_bin = (binning_dic_pop[count_pop]) 
+            limit_upper_pop = (current_pop_bin.partition('-')[2])
+            limit_lower_pop = (current_pop_bin.partition('-')[0])
+            #upper limit defined as number
+            if limit_upper_pop != '':
+                limit_upper_pop = int(limit_upper_pop)
+            # upper limit not defined set as 100
+            elif limit_upper_pop == '' or limit_upper_pop == ' ':
+                limit_upper_pop = int(100)
+            if limit_lower_pop != '':
+                limit_lower_pop = int(limit_lower_pop)
+
+           
+            if limit_upper_hosp > limit_upper_pop and limit_lower_hosp >= limit_lower_pop:
+                position = min(count_hosp, count_pop)
+                if current_pop_bin not in pop_age_bin_resort:
+                    pop_age_bin_resort.update({current_pop_bin:(position)})
+                if current_hosp_bin not in hosp_age_bin_resort:
+                    hosp_age_bin_resort.update({current_hosp_bin:(position)})
+                if current_pop_bin not in new_age_bins_resort: 
+                    new_age_bins_resort.append(current_hosp_bin)
+                count_pop += 1
+            
+            elif limit_upper_hosp > limit_upper_pop and limit_lower_hosp <= limit_lower_pop:
+                error_message = ('error: age bins cannot be resported')
+                break
+
+            elif limit_upper_pop > limit_upper_hosp and limit_lower_hosp >= limit_lower_pop:
+                position = min(count_hosp, count_pop)
+                if current_hosp_bin not in hosp_age_bin_resort:
+                    hosp_age_bin_resort.update({current_hosp_bin:(position)})
+                if current_pop_bin not in pop_age_bin_resort:
+                    pop_age_bin_resort.update({current_pop_bin:(position)})
+                if (current_hosp_bin or current_pop_bin) not in new_age_bins_resort: 
+                    new_age_bins_resort.append(current_pop_bin)
+                count_hosp += 1
+            
+            elif limit_upper_pop > limit_upper_hosp and limit_lower_hosp <= limit_lower_pop:
+                error_message = ('error: age bins cannot be resported')
+                break
+            elif limit_upper_hosp == limit_upper_pop:
+                position = min(count_hosp, count_pop)
+                if current_hosp_bin not in hosp_age_bin_resort:
+                    hosp_age_bin_resort.update({current_hosp_bin:(position)}) 
+                if current_pop_bin not in pop_age_bin_resort:
+                    pop_age_bin_resort.update({current_pop_bin:(position)})
+                if (current_hosp_bin or current_pop_bin) not in new_age_bins_resort: 
+                    new_age_bins_resort.append(current_pop_bin)
+                count_hosp += 1
+                count_pop += 1
+    if hosp_age_bin_resort == {} or pop_age_bin_resort == {} or new_age_bins_resort == []:
+        return error_message
+    else :
+        return  hosp_age_bin_resort, pop_age_bin_resort, new_age_bins_resort  
+
 # 2.1 load and chek json file 
 def load_covid_data(filepath):
 
@@ -47,11 +127,9 @@ def check_json_strucutre(str_input):
     else:
         output = ('Error: ', catagories, ' not found in the loaded data')
     return output
-covid_data = load_covid_data(current_filepath
 
 # 2.2 evolution cases by age group and total pop 
 def match_age_bins(binning_dic_hosp,binning_dic_pop):
-    # set/create variables to zzero and empty 
     count_hosp = 0
     count_pop = 0 
     hosp_age_bin_resort  = {}
@@ -59,7 +137,6 @@ def match_age_bins(binning_dic_hosp,binning_dic_pop):
     new_age_bins_resort = []
     len_binning_dic_pop  = len(binning_dic_pop)
     len_binning_dic_hosp = len(binning_dic_hosp)
-    # while still age bins present 
     while count_hosp < len_binning_dic_hosp and count_pop < len_binning_dic_pop:
             # upper limit for hospital age bin
             current_hosp_bin = (binning_dic_hosp[count_hosp]) 
@@ -87,7 +164,7 @@ def match_age_bins(binning_dic_hosp,binning_dic_pop):
             if limit_lower_pop != '':
                 limit_lower_pop = int(limit_lower_pop)
 
-           # if hosp larger add index to dictionary and move to next  pop age bin 
+           
             if limit_upper_hosp > limit_upper_pop and limit_lower_hosp >= limit_lower_pop:
                 position = min(count_hosp, count_pop)
                 if current_pop_bin not in pop_age_bin_resort:
@@ -97,11 +174,11 @@ def match_age_bins(binning_dic_hosp,binning_dic_pop):
                 if current_pop_bin not in new_age_bins_resort: 
                     new_age_bins_resort.append(current_hosp_bin)
                 count_pop += 1
-            # if lower limit not fit in bin - error message 
+            
             elif limit_upper_hosp > limit_upper_pop and limit_lower_hosp <= limit_lower_pop:
                 error_message = ('error: age bins cannot be resported')
-                break       
-            # pop bin larger index in dictionary and move to next hosp bin
+                break
+
             elif limit_upper_pop > limit_upper_hosp and limit_lower_hosp >= limit_lower_pop:
                 position = min(count_hosp, count_pop)
                 if current_hosp_bin not in hosp_age_bin_resort:
@@ -111,11 +188,10 @@ def match_age_bins(binning_dic_hosp,binning_dic_pop):
                 if (current_hosp_bin or current_pop_bin) not in new_age_bins_resort: 
                     new_age_bins_resort.append(current_pop_bin)
                 count_hosp += 1
-             # if lower limit not fit in bin - error message 
+            
             elif limit_upper_pop > limit_upper_hosp and limit_lower_hosp <= limit_lower_pop:
                 error_message = ('error: age bins cannot be resported')
                 break
-            # if limits same index and move to next bi nin hosp and pop
             elif limit_upper_hosp == limit_upper_pop:
                 position = min(count_hosp, count_pop)
                 if current_hosp_bin not in hosp_age_bin_resort:
@@ -126,12 +202,10 @@ def match_age_bins(binning_dic_hosp,binning_dic_pop):
                     new_age_bins_resort.append(current_pop_bin)
                 count_hosp += 1
                 count_pop += 1
-    # return error message if not fill dicoary
     if hosp_age_bin_resort == {} or pop_age_bin_resort == {} or new_age_bins_resort == []:
         return error_message
-    # reutrn edicronary and bins 
     else :
-        return  hosp_age_bin_resort, pop_age_bin_resort, new_age_bins_resort
+        return  hosp_age_bin_resort, pop_age_bin_resort, new_age_bins_resort  
 
 def cases_per_population_by_age(input_data):
 
